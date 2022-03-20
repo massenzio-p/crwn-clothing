0a7bcae3e6f071fece6f9273201bfcb3299299bb
Max Pestov <pestov.maxik@gmail.com>
2022-03-20 13:42:21 +0500

    project  deps upgraded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ShopPage from "./pages/homepage/shop/shoppage.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
@@ -36,11 +36,11 @@ class App extends React.Component {
         return (
             <div className="App">
                 <Header />
-                <Switch>
+                <Routes>
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/shop' component={ShopPage}/>
                     <Route path='/signIn' component={SignInAndSignUpPage}/>
-                </Switch>
+                </Routes>
             </div>
         )
     };
